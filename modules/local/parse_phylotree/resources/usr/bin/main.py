fe424f6bcc932f290def7a71f1207de4a2b66161
Merlin Szymanski <merlin_szymanski@eva.mpg.de>
2026-01-12 19:58:17 +0100

    Remodel the parsing

--- modules/local/parse_phylotree/resources/usr/bin/main.py
+++ modules/local/parse_phylotree/resources/usr/bin/main.py
@@ -5,10 +5,7 @@ from anytree.render import AsciiStyle
 from xml.dom.minidom import parse
 import sys
 import re
-
-#
-# This is aweful spaghetti-code and I am sorry for this!!
-#
+import copy
 
 def check_position_coverage(poly, data, all_parent_positions=[]):
     def return_uncovered():
@@ -41,131 +38,168 @@ def check_position_coverage(poly, data, all_parent_positions=[]):
     ##
     return perc, target, cov
 
+
+#
+# This is aweful spaghetti-code and I am sorry for this!!
+#
+
+# define input
+xml_path = sys.argv[1]
+pileup_path = sys.argv[2]
 prefix = sys.argv[3]
 
 # open XML file
-with open(sys.argv[1]) as xml_file:
-    tree = parse(xml_file)
+with open(xml_path) as xml_file:
+    xml_tree = parse(xml_file)
 
 # open pileup file
-with open(sys.argv[2]) as pileup_file:
-    pileup_data = {}
-    for line in pileup_file:
-        cols = line.split('\t')
-        sequence = cols[4]
-        quality = cols[5]
-        good_bases = ''.join([base for base, q in zip(sequence, quality) if q != '!'])
+pileup_data = {}
+with open(pileup_path) as pileup_file:
+    for _line in pileup_file:
+        _cols = _line.split('\t')
+        _sequence = _cols[4]
+        _quality = _cols[5]
+        # this line ignores the masked bases at the end
+        _good_bases = ''.join([b for b, q in zip(_sequence, _quality) if q != '!'])
         # import position -> bases
-        pileup_data[cols[1]] = good_bases.upper()
+        pileup_data[_cols[1]] = _good_bases.upper()
 
-# create the first tree
-node = AnyNode(id='mtMRCA', parent=None, data=[])
+# create the anynode tree
+# create dictionaries to be filled during parsing
+raw_data = {
+    'branch_positions_covered': 0,
+    'branch_positions_support': 0,
+    'node_positions_covered': 0,
+    'node_positions_support': 0,
+    'branch_positions': [],
+    'node_positions': [], #only positions from this haplogroup node
+    'node_positions_rendered': [], #node_positions, but including the read coverage statistics
+    'penalty':-1 #for branches at the leaves, update later
+}
+
+node = AnyNode(id='mtMRCA', parent=None, data=raw_data.copy())
 name_node_dict = {'mtMRCA':node}
-node_parent_polys = {'mtMRCA':set()}
 
-
-# walk through the XML and fill the tree
-for haplogroup in tree.getElementsByTagName('haplogroup'):
-    name = haplogroup.getAttribute('name')
+# walk through the XML and fill the anynode-tree
+for xml_haplogroup in xml_tree.getElementsByTagName('haplogroup'):
+    # thats the haplogroup label
+    name = xml_haplogroup.getAttribute('name')
     
-    # get the haplogroup name from the XML parent
-    parent_node = haplogroup.parentNode
-    if parent_node.nodeType == parent_node.ELEMENT_NODE and parent_node.tagName == 'haplogroup':
-        parent = parent_node.getAttribute('name')
+    # get the parent haplogroup name from the XML parent
+    # to get the AnyTree node parent from the dict
+    xml_parent_node = xml_haplogroup.parentNode
+    if xml_parent_node.tagName == 'haplogroup':
+        parent = xml_parent_node.getAttribute('name')
     else:
         continue
-
-    # overwrite parent_node with the anytree Node element
-    parent_node = name_node_dict[parent]
     
-    # get all positions that are checked in the same branch
-    # this is to double check if a position was aleady found within the same branch
-    # e.g L3: 16311T --> U1b3: 16311T!
-    all_parent_positions = node_parent_polys[parent_node.id]
+    # Now get the actual parent-node (AnyTree) from the dict 
+    parent_node = name_node_dict[parent]
 
-    data = []
-    found_covered = 0
-    found_target = 0
-    for child in haplogroup.childNodes: #child here means XML childs --> for parsing the POLYs
-        if child.nodeType == child.ELEMENT_NODE and child.tagName == 'details':
+    # Add the data-dict for the current haplogroup node
+    data = copy.deepcopy(raw_data)
+    data.update({
+        'branch_positions_covered': parent_node.data['branch_positions_covered'], # later: add node covered on top 
+        'branch_positions_support': parent_node.data['branch_positions_support'], # later: add node support on top
+        'branch_positions': parent_node.data['branch_positions'].copy(), # later: add node_positions on top
+        # add accumulated read-support as extra column? 
+    })
+
+    # Now parse all the positions (poly-tags) and update the dictionary
+    for xml_child in xml_haplogroup.childNodes: #child here means XML childs --> for parsing the POLYs
+        if xml_child.nodeType == xml_child.ELEMENT_NODE and xml_child.tagName == 'details':
         # Get data for each haplogroup-defining position 
         # Get the 'poly' elements directly under the 'details' element
-            polys = []
-            raw_polys = []
-            for poly in child.getElementsByTagName('poly'):
+            for xml_poly in xml_child.getElementsByTagName('poly'):
                 # extract position from XML
-                poly_data = poly.firstChild.data
-                raw_polys.append(poly_data)
-                # now parse the positions and calculate cpverage
-                perc, target, cov = check_position_coverage(poly_data, pileup_data, all_parent_positions)
-                poly_string = f"{poly_data} ({perc:.2f}% {target}/{cov})"
+                # poly = e.g. 1234T
+                poly = xml_poly.firstChild.data
+                data['node_positions'].append(poly)
+                data['branch_positions'].append(poly)
+
+                # now parse the positions and calculate coverage
+                perc, target, cov = check_position_coverage(poly, pileup_data, parent_node.data['branch_positions'])
+
+                parsed_poly = f"{poly} ({perc:.2f}% {target}/{cov})"
+                data['node_positions_rendered'].append(parsed_poly)
+
                 if cov > 0:
-                    found_covered += 1
+                    data['node_positions_covered'] += 1
+                    data['branch_positions_covered'] += 1
                 if perc > 10:
-                    found_target += 1
-                # save for later 
-                polys.append(poly_string)
-            data.append(polys)
-            # now update the all_parent_positions to store them in the dict for all descendent haplogroups
-            tmp = all_parent_positions.copy()
-            tmp.update(raw_polys)
-            node_parent_polys.update({name: tmp})
-    #add node-data to the tree
-    data.append([found_target, found_covered])
-    # and add node-data for later overwriting of the stats for each node (including leaves)
-    data.append([found_target, found_covered])
-    # add penalty-value for later updating
-    data.append(1)
-    #create the tree
+                    data['node_positions_support'] += 1
+                    data['branch_positions_support'] += 1
+
+    #add node to the tree
     tmp = AnyNode(id=name, parent=parent_node, data=data)
     name_node_dict.update({name:tmp})   
 
 # now summarize stats on each node
 for hap in PostOrderIter(node):
-    #walk from the leaves up and count the number of successful (leave) haplogroups on each node:
-    #thats what the PostOrderIter does
-    total_target = sum(x.data[2][0] for x in hap.children)
-    total_cov = sum(x.data[2][1] for x in hap.children)
-    if total_cov == 0:
+    #walk from the leaves up and set the penalty 
+    if hap.data['node_positions_support'] != 0:
+        hap.data['penalty'] = 0
         continue
-    if len(hap.data) < 3:
-        hap.data=[total_target, total_cov]
-        continue # back to root
-    # introduce a penalty for nodes in a branch that are
-    # covered, but dont have the target allel
-    #   Tree                       Node   Position  Penalty
-    #  +-- L1c3b'c                 1/20   0/1       4 (Distance to Tip)
-    #      |-- L1c3b               1/22   0/2       3 (Distance to Tip)
-    #      |   |-- L1c3b1          1/4    0/3       2 (Distance to Tip)
-    #      |   |   |-- L1c3b1a     1/2    1/2       1 (Tip)
-    #      |   |   +-- L1c3b1b     0/2    0/2       1 (Tip)
-    #      |   +-- L1c3b2          0/7    0/7       1 (Tip)
-    #      +-- L1c3c               0/9    0/9       1 (Tip)
-    #
-    # So get the minimum Penalty from the children (if it has node-coverage)
-    try:
-        child_penalty = min(x.data[3] for x in hap.children if x.data[2][0] != 0)
-    except:
-        child_penalty = 1 # no counts on the nodes will be filtered out anyways later...
-    if  hap.data[1][0] == 0:
-        penalty = child_penalty + 1
-    else:
-        penalty = 1
+    if hap.id == 'mtMRCA': # root
+        hap.data['penalty'] = 0
+        continue
 
-    hap.data[3] = penalty
-    hap.data[2] = [total_target, total_cov]
+    # introduce the penalty for nodes in a branch so that
+    # covered, but dont have the target allel
+    #   Tree                       Branch   Position  Penalty
+    #  +-- L1c3b'c                 0/1      0/1       3 (Distance to covered tip
+    #      |-- L1c3b               0/3      0/2       2 (Distance to covered tip)
+    #      |   |-- L1c3b1          0/6      0/3       1 (Distance to covered tip)
+    #      |   |   |-- L1c3b1a     1/8      1/2       0 (Tip)
+    #      |   |   +-- L1c3b1b     0/8      0/2       -1 (Tip)
+    #      |   +-- L1c3b2          0/10     0/7       -1 (Tip)
+    #      +-- L1c3c               0/10     0/9       -1 (Tip)
+    #
+    # For each node, get the minimum Penalty from the children
+    # keep -1 if -1 else add plus 1
+    try:
+        # check if a children exists that doesnt have -1 as penalty
+        child_penalty = min(x.data['penalty'] for x in hap.children if x.data['penalty'] != -1)
+    except:
+        # keep the penalty at -1
+        # set to -2, so that adding 1 makes it -1 again :D
+        child_penalty = -2
+        
+    hap.data['penalty'] = child_penalty + 1
 
 def print_header(file):
-    print('PhyloTree\tNodeCoverage\tPositionCoverage\tPenalty\tReadCoverage', file=file)
+    print('\t'.join(
+        [
+            'Haplogroup',
+            'Parent',
+            'PhyloTree',
+            'BranchSupport',
+            'BranchSupportPercent',
+            'PositionSupport',
+            'Penalty',
+            'ReadSupport'
+        ]
+    ), file=file)
 
 def print_line(row, file):
+    if row.node.data['branch_positions_covered'] > 0:
+        branch_support = row.node.data['branch_positions_support']/row.node.data['branch_positions_covered'] * 100
+    else:
+        branch_support = 0
+    if row.node.parent:
+        parent = row.node.parent.id
+    else:
+        parent = '-'
     print('\t'.join(
             [
+                row.node.id,
+                parent,
                 f"{row.pre.rstrip()} {row.node.id}",
-                f"{row.node.data[2][0]}/{row.node.data[2][1]}",
-                f"{row.node.data[1][0]}/{row.node.data[1][1]}",
-                f"{row.node.data[3]}",
-                f"{'; '.join(row.node.data[0])}"
+                f"{row.node.data['branch_positions_support']}/{row.node.data['branch_positions_covered']}",
+                f"{branch_support:.2f}%",
+                f"{row.node.data['node_positions_support']}/{row.node.data['node_positions_covered']}",
+                f"{row.node.data['penalty']}",
+                f"{'; '.join(row.node.data['node_positions_rendered'])}"
             ]
         ), file=file
     )
@@ -174,66 +208,76 @@ def print_line(row, file):
 rendered = list(RenderTree(node, style=AsciiStyle))
 
 # First, print stats for every haplogroup!
-with open(f"{prefix}.01_stats_all_groups.tsv", 'w') as tree1:
+with open(f"{prefix}.tree_all_groups.tsv", 'w') as tree1:
     print_header(tree1)
     for row in rendered:
-        if len(row.node.data) > 2: #any node in the middle
-            print_line(row, tree1)
-        else: # root
-            print(f"{row.pre.rstrip()} {row.node.id}\t{row.node.data[0]}/{row.node.data[1]}\t\t", file=tree1)
-
-#Then, print the BEST path
-# create a new tree that only consists of the best nodes
-best_node = AnyNode(id='mtMRCA', parent=None, data=node.data)
-
-def get_best_child(node, best_parent):
-    #The penalty is _really_ strong e.g. Penalty of 2 -> cut node-count in half 
-    order = sorted(node.children, key=lambda x: x.data[2][0]/x.data[3])
-    try:
-        best_child = order[-1]
-        tmp = AnyNode(id=best_child.id, parent=best_parent, data=best_child.data)
-        return get_best_child(order[-1], tmp)
-    except IndexError: #reached the tip
-        pass
-
-get_best_child(node, best_node)
-
-best_path = list(RenderTree(best_node, style=AsciiStyle))
-
-with open(f"{prefix}.03_stats_best_path.tsv", 'w') as tree3:    
-    print_header(tree3)
-    for row in best_path:
-        try:
-            if row.node.data[2][0] == 0:
-                continue #the last tip is random and could be empty...
-            print_line(row, tree3)
-        except IndexError: # root
-            print(f"{row.pre.rstrip()} {row.node.id}\t{row.node.data[0]}/{row.node.data[1]}\t\t", file=tree3)
+        print_line(row, tree1)
 
 #Then, prune the tree, so that:
+# - nodes with less than 70% branch support are removed
 # - nodes with 0 coverage are removed
-# - nodes with penalty 3 or more are removed
+# - nodes with penalty 4 or more are removed (actually, this is now mostly driven by the branch support...)
 # 
 # iterate through the tree and set all parents to None if these conditions apply
 
 for hap in PostOrderIter(node):
     #walk from the leaves up and count the number of successful (leave) haplogroups on each node:
     #thats what the PostOrderIter does
-    if len(hap.data) < 3:
+    if hap.id == 'mtMRCA':
         continue # root
+    
+    if hap.data['branch_positions_covered'] > 0:
+        branch_support = hap.data['branch_positions_support']/hap.data['branch_positions_covered'] * 100
+    else:
+        branch_support = 0
 
-    total_coverage = hap.data[2][0]
-    penalty = hap.data[3]
-
-    if total_coverage == 0 or penalty >= 4:
+    if branch_support < 70 or hap.data['penalty'] >= 4 or hap.data['penalty'] == -1:
+        hap.parent = None
+    
+    # if a node was filtered because of too little support, also remove unsupported intermediate nodes
+    if hap.data['node_positions_support'] == 0 and len(hap.children)==0:
         hap.parent = None
 
 prune_rendered = list(RenderTree(node, style=AsciiStyle))
-with open(f"{prefix}.02_stats_all_groups_with_coverage.tsv", 'w') as tree2:    
+with open(f"{prefix}.tree_70perc_support.tsv", 'w') as tree2:    
     print_header(tree2)
     for row in prune_rendered:
-        try:
-            test = row.node.data[2][0]
-            print_line(row, tree2)
-        except IndexError: # root
-            print(f"{row.pre.rstrip()} {row.node.id}\t{row.node.data[0]}/{row.node.data[1]}\t\t", file=tree2)
+        print_line(row, tree2)
+
+#
+#
+# Do the same thing again, but now for 80% support and 3 gaps only
+#
+#
+
+
+#Then, prune the tree, so that:
+# - nodes with less than 70% branch support are removed
+# - nodes with 0 coverage are removed
+# - nodes with penalty 4 or more are removed (actually, this is now mostly driven by the branch support...)
+# 
+# iterate through the tree and set all parents to None if these conditions apply
+
+for hap in PostOrderIter(node):
+    #walk from the leaves up and count the number of successful (leave) haplogroups on each node:
+    #thats what the PostOrderIter does
+    if hap.id == 'mtMRCA':
+        continue # root
+    
+    if hap.data['branch_positions_covered'] > 0:
+        branch_support = hap.data['branch_positions_support']/hap.data['branch_positions_covered'] * 100
+    else:
+        branch_support = 0
+
+    if branch_support < 70 or hap.data['penalty'] >= 3:
+        hap.parent = None
+    
+        # if a node was filtered because of too little support, also remove unsupported intermediate nodes
+    if hap.data['node_positions_support'] == 0 and len(hap.children)==0:
+        hap.parent = None
+
+tree3_render = list(RenderTree(node, style=AsciiStyle))
+with open(f"{prefix}.tree_70perc_support_3gaps.tsv", 'w') as tree3:    
+    print_header(tree3)
+    for row in tree3_render:
+        print_line(row, tree3)
